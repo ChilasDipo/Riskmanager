b0d4a0e6611185bd0262cd4926a4bee506826a18
ChilasDipo <Chil0041@edu.easj.dk>
2020-09-14 10:55:24 +0200

    Begyndte opgave areal beregning af rektangel

---
+++ src/Kapitel_2/Areal_af_rektangel.java
@@ -0,0 +1,15 @@
+package Kapitel_2;
+
+public class Areal_af_rektangel {
+    public static void main(String[] args) {
+
+        System.out.println(Beregner(2,5));
+        System.out.println(Beregner(6,12));
+    }
+
+    public static int Beregner(int længde1 , int længde2) {
+
+    return Arealet = længde1 * længde2;
+    }
+
+}
